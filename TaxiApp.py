ae8f6b0988f0a21a9814f8599e726a611c433b8f
PramukaNavodh <pramunavodh@gmail.com>
2025-10-21 10:39:44 +0530

    complete system with simulation

--- TaxiApp.py
+++ TaxiApp.py
@@ -26,21 +26,19 @@ def passenger(env, name, driver_pool, ride_time_mean, reposition_mean, wait_time
         yield env.timeout(reposition_time)
         total_busy_time[0] += reposition_time
 
-#driver
-def driver(env, name, driver_pool):
-    while True:
-        yield driver_pool
+# #driver
+# def driver(env, name, driver_pool):
+#     while True:
+#         yield driver_pool
 
 #taxi system
-def system(env, arrival_rate, num_drivers, ride_time_mean, reposition_mean, sim_duration):
+def system(env, arrival_rate, num_drivers, ride_time_mean, reposition_mean, sim_duration,wait_times,queue_at_arrivals,
+           total_busy_time):
     driver_pool = simpy.Resource(env, capacity = num_drivers);
-    wait_time = []
-    queue_at_arrivals = []
-    total_busy_time = [0.0]
 
-    #spawning individual driver processes
-    for i in range(num_drivers):
-        env.process(driver(env,f'Driver-{i}',driver_pool))
+    # #spawning individual driver processes
+    # for i in range(num_drivers):
+    #     env.process(driver(env,f'Driver-{i}',driver_pool))
 
     #generating drivers
     while True:
@@ -48,9 +46,25 @@ def system(env, arrival_rate, num_drivers, ride_time_mean, reposition_mean, sim_
         yield env.timeout(inter_arrival)
         if env.now >= sim_duration:
             break
-        env.process(rider(env, f'Rider-{len(wait_times)+1}', driver_pool,ride_time_mean,reposition_mean,
+        env.process(passenger(env, f'Passenger-{len(wait_times)+1}', driver_pool,ride_time_mean,reposition_mean,
                           wait_times, queue_at_arrivals, total_busy_time))
         
+
+
+
+def run_simulation (arrival_rate, num_drivers,ride_time_mean = 15, reposition_mean = 2,
+                sim_duration = 480, seed = 42):
+    random.seed(seed)
+    env = simpy.Environment()
+
+    wait_times = []
+    queue_at_arrivals = []
+    total_busy_time = [0.0]
+
+    env.process(system(env,arrival_rate,num_drivers,ride_time_mean,reposition_mean,sim_duration,wait_times,queue_at_arrivals,
+                       total_busy_time))
+    env.run(until = sim_duration)
+
     #calculatiion of metrics
     avg_wait = statistics.mean(wait_times) if wait_times else 0
     max_queue = max(queue_at_arrivals) if queue_at_arrivals else 0
@@ -68,19 +82,24 @@ def system(env, arrival_rate, num_drivers, ride_time_mean, reposition_mean, sim_
 
     }
 
-
-def simulation (arrival_rate, num_drivers,ride_time_mean = 15, reposition_mean = 2,
-                sim_duration = 480, seed = 42):
-    random.seed(seed)
-    env = simpy.Environment()
-
-    def sim_gen():
-        yield from system(env, arrival_rate,num_drivers,ride_time_mean,reposition_mean,sim_duration)
-    results = env.run(until=sim_duration, generator = sim_gen)
-    return results;
+    # def sim_gen():
+    #     yield from system(env, arrival_rate,num_drivers,ride_time_mean,reposition_mean,sim_duration)
+    # results = env.run(until=sim_duration, generator = sim_gen)
+    # return results;
 
 
 #case 1
 if __name__ == "__main__":
-    results = run_simulation(arrival_rate=2.0, num_drivers=5, ride_time_mean=15, sim_duration=480)
-    print(results)
+    results = run_simulation(arrival_rate=0.5, num_drivers=5, ride_time_mean=15, sim_duration=480)
+    print("Simulation results - ")
+    for k, v in results.items():
+        if isinstance(v, (float, int)):
+            print(f"{k}: {v:.2f}")
+
+
+#visualization
+plt.plot(results['queue_at_arrivals'])
+plt.xlabel("Passenger arrivals")
+plt.ylabel("Queue lemgth")
+plt.title("Queue lenth at passenger arrival")
+plt.show()
